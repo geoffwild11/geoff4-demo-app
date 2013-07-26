69513be106d433df5902c78e694a72cdc326368e
Geoff Wild <geoff.wild@snc.edu>
2013-07-25 19:02:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geoff4SampleApp::Application.config.secret_key_base = '5288fb24ffceeb0b5438eea8bd7b37ae2ff979c9e0e59989abebb559b7ba10934d5d0b9b1f87b371f2c2d3e9e5e5317755e47dffb36edcdee7caf3da7ad1bc35'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Geoff4SampleApp::Application.config.session_store :cookie_store, key: '_geoff4-sample-app_session'
